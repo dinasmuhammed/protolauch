77424db8a3ca41316dba3cd3271af4eaaa0363c3
dinasmuhammed <dinaftima@proton.me>
2024-09-24 22:09:14 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="mt-8 pt-8 border-t border-gray-800 text-center">
-          <p>&copy; 2023 ProtoLaunch. All rights reserved.</p>
+          <p>&copy; 2024 ProtoLaunch. All rights reserved.</p>
         </div>
       </div>
     </footer>
